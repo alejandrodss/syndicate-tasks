edc0cc549470e27a0601d125a17a3a76305a681d
Alejandro Duarte <alejandro_duarte@epam.com>
2024-06-03 17:32:36 -0500

    Add task2 part 2

--- hello-lambda/app/lambdas/hello_world/index.js
+++ hello-lambda/app/lambdas/hello_world/index.js
@@ -1,8 +1,13 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        message: "Hello from Lambda"
-    };
-    return response;
+    const { requestContext: { http: { method, path } } } = event;
+    if(method === "GET" && path === "/hello") {
+        return ({
+            statusCode: 200,
+            body: "{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}"
+        });
+    }
+    return({
+        statusCode: 400,
+        body: `{"statusCode": 400, "message": "Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}"}`
+    });
 };
